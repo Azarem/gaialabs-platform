55d0f2a72b574cc8587b47700d55620ab75d6b02
Azarem <Azarem@users.noreply.github.com>
2025-07-17 17:53:16 -0400

    fix BlockReader test async

--- packages/gaia-core/src/rom/extraction/__tests__/BlockReader.test.ts
+++ packages/gaia-core/src/rom/extraction/__tests__/BlockReader.test.ts
@@ -1,14 +1,20 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeAll } from 'vitest';
 import { OpCode, ALL_OPCODES, GROUPED_OPCODES, OpCodeUtils } from '../OpCode';
 import { AddressingMode, DbRootUtils } from 'gaia-shared';
 import { BlockReader } from '../blocks';
 
+
 describe('BlockReader', () => {
+  let reader: BlockReader;
 
-    const data = [];
-    const dbRoot = DbRootUtils.fromFolder("../../../../../../ext/GaiaLib/db", "../../../../../../ext/GaiaLib/db/snes");
-
-    const reader = new BlockReader(data, dbRoot);
+  beforeAll(async () => {
+    const data = new Uint8Array();
+    const dbRoot = await DbRootUtils.fromFolder(
+      '../../ext/GaiaLib/db/us',
+      '../../ext/GaiaLib/db/snes'
+    );
+    reader = new BlockReader(data, dbRoot);
+  });
 
   describe('BlockReader class', () => {
     it('should create a block reader with correct properties', () => {
