a9844d9604d6584fa716fccc4f09a837f01e70ed
Bryan Moulton <azarem@gmail.com>
2025-07-20 10:45:43 -0400

    dummy test

---
+++ apps/gaia-api/src/__TEST__/dummy.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest';
+
+
+describe('Testing Gaia API', () => {
+
+  describe('True', () => {
+    it('should be true', () => {
+      expect(true).toBe(true);
+    });
+  });
+
+}); 
