92358bcbda22e6f6fbabcafa3569cb7974b6e5d7
Azarem <Azarem@users.noreply.github.com>
2025-08-06 06:16:55 -0400

    chore(ui): convert tailwind config to esm

--- packages/gaia-ui/tailwind.config.js
+++ packages/gaia-ui/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import tailwindcssAnimate from "tailwindcss-animate";
+
+export default {
   darkMode: ["class"],
   content: [
     './src/**/*.{ts,tsx}',
@@ -165,5 +167,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
