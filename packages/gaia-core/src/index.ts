fbe4927522903c8fed87a62b409bd8d53c4c2bd7
Bryan Moulton <azarem@gmail.com>
2025-09-06 23:45:42 -0400

    Fix core

--- packages/gaia-core/src/index.ts
+++ packages/gaia-core/src/index.ts
@@ -26,7 +26,7 @@ export * from './sprites';
  * - Compression: QuintetLZ compression algorithm
  * - Sprites: Sprite animation system
  */
-export const GAIA_CORE_VERSION = '0.1.1';
+export const GAIA_CORE_VERSION = '0.1.2';
 
 // Platform detection
 export const isPlatformBrowser = typeof window !== 'undefined';
